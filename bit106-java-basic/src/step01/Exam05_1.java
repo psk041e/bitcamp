4b9afc93595c931331226ec0835b931052ba42d4
psk041e <tjrud64@gmail.com>
2018-03-02 14:04:52 +0900

    '.'

---
+++ bit106-java-basic/src/step01/Exam05_1.java
@@ -0,0 +1,20 @@
+package step01;
+// 정수의 리터럴
+public class Exam05_1 {
+    public static void main(String[] args) {
+        // 10진수
+        System.out.println(100);
+
+        // 8진수
+        // - 0으로 시작한다.
+        System.out.println(0144);
+
+        // 2진수
+        // - 0b 또는 0B로 시작한다.
+        System.out.println(0b1100100);
+
+        // 16진수
+        // - 0x 또는 0X 로 시작한다.
+        System.out.println(0x64);
+    }
+}
---
+++ bit106-java-basic/src/step01/Exam05_4.java
@@ -0,0 +1,8 @@
+package step01;
+// 정수의 리터럴 - 메모리에 저장하는 방법
+public class Exam05_4 {
+    public static void main(String[] args) {
+        System.out.println(5 - 3);
+        System.out.println(5 + (-3));
+    }
+}
