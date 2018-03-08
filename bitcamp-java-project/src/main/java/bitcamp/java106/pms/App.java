478f1b086a19db4fee42cd1ca0ea20270441db72
psk041e <tjrud64@gmail.com>
2018-03-08 11:23:34 +0900

    '.'

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -39,57 +39,80 @@
 */
 
 package bitcamp.java106.pms;
+
+// 컴파일러에게 클래스의 위치 정보를 알려준다.
+// => 컴파일한 후 import 명령은 제거된다.
+//    즉 .class 파일에 포함되지 않는다.
+// => **** 그러니 import 문장이 많으면 .class 파일이 커지지 않을까 걱정말라! ****
+//    (자바 버추얼 머신이 아니라 컴파일러에게 제공하는 것이다)
+import bitcamp.java106.pms.domain.Team;
+import java.util.Scanner;
+
 // next() 와 nextLine()의 차이
+
+// 1단계: 
+// 2단계: 배열 적용
+// 3단계: 반복문 적용
+// 4단계: 조건문 적용
 public class App {
     public static void main(String[] args){
-        java.io.InputStream keyboard = System.in;
-        java.util.Scanner keyScan = new java.util.Scanner(keyboard);
+        Scanner keyScan = new Scanner(System.in);
+        // 매일 하루종일 돌아가는 프로그램에서는 keyScan.close를 해 주어야
+        // garbage가 생기지 않지만 짧은 소스코드에서는
+        // 자바 버추얼머신이 종료하면 어차피 자원을 다 반납하기 때문에
+        // 써주지 않아도 된다.
+        
+        // 팀 정보를 받을 메모리 준비
+        // S => 인스턴스의 주소를 저장하는 레퍼런스 
+        //      (teamRef에서 Ref는 생략해서 사용한다.)
+        Team[] teams = new Team[5]; 
 
-        class Source {
-            String teamName;
-            String description;
-            int maxQty;
-            String startDate;
-            String endDate;
-        }
 
-
-        Source[] s = new Source[5];
-
-        int su = 1;
-        for (int i = 0; i < 5; i++){
-            s[i] = new Source();
+        int count = 0;
+        for (int i = 0; i < teams.length; i++){ // index의 약자로써 i를 사용
+            count++;
+            teams[i] = new Team(); 
+            // 배열에 팀 객체를 넣어라 (객체의 주소를 넣어라)
+            // 주소는 주고받을수 있어도 객체는 주고받을 수 없다.
             System.out.print("팀명? ");
-            s[i].teamName = keyScan.nextLine();
+            teams[i].name = keyScan.nextLine();
             System.out.print("설명? ");
-            s[i].description = keyScan.nextLine();
+            teams[i].description = keyScan.nextLine();
             System.out.print("최대인원? ");
-            s[i].maxQty = keyScan.nextInt();
-            keyScan.nextLine(); // 숫자 뒤에 줄바꿈 코드르르 읽는다.
-                                // 일고 난 뒤에 아무것도 안하기 때문에
+            teams[i].maxQty = keyScan.nextInt();
+            keyScan.nextLine(); // 숫자 뒤에 줄바꿈 코드를 읽는다.
+                                // 읽고 난 뒤에 아무것도 안하기 때문에
                                 // 일종의 줄바꿈 코드를 제거하는 효과가 있다.
             System.out.print("시작일? ");
-            s[i].startDate = keyScan.nextLine();
+            teams[i].startDate = keyScan.nextLine();
             System.out.print("종료일? ");
-            s[i].endDate = keyScan.nextLine();
+            teams[i].endDate = keyScan.nextLine();
             
-            System.out.print("계속 입력 하시겠습니까?(y/n) ");
-            String answer = keyScan.nextLine();
+            System.out.print("계속 입력 하시겠습니까?(Y/n) ");
+            String str = keyScan.nextLine();
+            // 메모리를 더 희생시키더라도
+            // 개발자가 코드를 봤을때 반복문 안에서만 쓰는 변수라는것을 알수있다
+            // 컴파일러가 알아서 옵티마이징한다 - 최소화
 
-            if (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n")) {
-                System.out.println("알맞은 값이 아닙니다. 프로그램을 종료합니다! ");
-                break;
-            } else if (!answer.equalsIgnoreCase("y")) {
+            if(count == teams.length) { 
+                // => if (i == teams.length - 1) 과 같다.
+                // 배열의 끝에 도달하면 질문하지 않고 종료
                 break;
             }
-            su++;
+
+            if (str.toLowerCase().equals("n")) {
+                // .toLowerCase() : 입력받은 문자열을 대문자든 소문자든 소문자로
+                // 만들기 때문에 대문자를 넣어도 동일하게 돌아간다.
+                break;
+            }
         }
 
         System.out.println("-------------------------");
         
-        for (int i = 0; i < su; i++) {
-            System.out.println(s[i].teamName + ", " + s[i].description + ", "
-                + s[i].maxQty + "명, " + s[i].startDate + " ~ " + s[i].endDate);
+        for (int i = 0; i < count; i++) {
+            System.out.printf("%s, %d명, %s ~ %s\n",
+            teams[i].name, teams[i].maxQty, 
+            teams[i].startDate, teams[i].endDate);
         }
 
     }
